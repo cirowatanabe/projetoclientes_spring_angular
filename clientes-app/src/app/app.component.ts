425db7288d262e4cd26decc5a9f42e22ad2383c9
Ciro Watanabe <cw.artiga@gmail.com>
2023-10-11 11:10:17 -0300

    criação do projeto front end, módulo de template e de clientes e configuração de rotas

---
+++ clientes-app/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, AfterViewInit } from '@angular/core';
+
+import * as $ from 'jquery'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewInit {
+  title = 'clientes-app';
+
+  ngAfterViewInit(){
+    (function($) {
+      "use strict";
+  
+      var path = window.location.href; 
+          $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+              if (this instanceof HTMLAnchorElement && this.href === path) {
+                  $(this).addClass("active");
+              }
+          });
+  
+      // Toggle the side navigation
+      $("#sidebarToggle").on("click", function(e) {
+          e.preventDefault();
+          $("body").toggleClass("sb-sidenav-toggled");
+      });
+  })(jQuery);
+  }
+}
